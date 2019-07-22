e33abffdd557b32f6fbad4bc293a7cd4de72f338
ermyboy <3rmyboy@gmail.com>
2019-07-22 10:07:21 +0200

    Update2

---
+++ LAB&CTF/hackthebox/Challenges/Emdee five for life/Emdee_five.py
@@ -0,0 +1,29 @@
+#!usr/bin/env python
+import requests
+import hashlib
+import re
+
+
+url="http://docker.hackthebox.eu:42899//"
+
+r=requests.session()
+out=r.get(url)
+print (out)
+print ("***********************************************************")
+out=re.search("<h3 align='center'>+.*?</h3>",out.text)
+print (out)
+print ("***********************************************************")
+out=re.search(">.*<",out[0])
+print (out[0])
+print ("***********************************************************")
+out=re.search("[^|'|>|<]...................",out[0])
+print (out[0])
+print ("***********************************************************")
+out=hashlib.md5(out[0].encode('utf-8')).hexdigest()
+
+print("sending md5 :-{}".format(out))
+
+data={'hash': out}
+out = r.post(url = url, data = data)
+
+print(out.text)
